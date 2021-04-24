026f968f17283afc1278356177c1dec706a1ede6
kanzler <nicholas.kanzler@gmial.com>
2021-04-24 15:40:43 +0300

    add humanTurn() and aiTurn()

--- src/lesson4/Lesson4.java
+++ src/lesson4/Lesson4.java
@@ -1,6 +1,8 @@
 package lesson4;
 
 import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Lesson4 {
     private static final int SIZE = 3;
@@ -8,28 +10,61 @@ public class Lesson4 {
     private static final char DOT_O = 'O';
     private static final char DOT_EMPTY = '•';
     private static final char[][] map = new char[SIZE][SIZE];
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final Random random = new Random();
 
     public static void main(String[] args) {
         initMap();
         printMap();
+        humanTurn();
+        aiTurn();
+        printMap();
+    }
+
+    private static void aiTurn() {
+        int x;
+        int y;
+        do {
+            x = random.nextInt(SIZE);
+            y = random.nextInt(SIZE);
+        } while (map[x][y] != DOT_EMPTY);
+        map[x][y] = DOT_O;
+    }
+
+    private static void humanTurn() {
+        int x = -1;
+        int y = -1;
+        do {
+            System.out.println("Введите координаты X и Y");
+            x = scanner.nextInt() - 1;
+            y = scanner.nextInt() - 1;
+        }while (!isCellValid(x,y));
+        map[x][y] = DOT_X;
+
+    }
+
+    private static boolean isCellValid(int x, int y) {
+        if (x <0 || y < 0 || x >= SIZE || y >= SIZE) return false;
+        else return map[x][y] == DOT_EMPTY;
     }
 
     private static void initMap() {
-        for(char[] el : map){
-            Arrays.fill(el,DOT_EMPTY);
+        for (char[] el : map) {
+            Arrays.fill(el, DOT_EMPTY);
         }
     }
 
     private static void printMap() {
         printHeader();
         printBody();
+        System.out.println();
     }
 
     private static void printBody() {
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE + 1; j++) {
                 if (j == 0) {
-                    System.out.print(i + 1 + "\t");
+                    System.out.print(i + 1 + "\t");             // табуляция для того,чтобы сетка не съежала при SIZE >= 10
                 } else {
                     System.out.print(map[i][j - 1] + "\t");
                 }
