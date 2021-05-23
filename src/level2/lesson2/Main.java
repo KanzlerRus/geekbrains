9929b15188fe94c8cc8d0d54bc81e93dcd761e9d
kanzler <nicholas.kanzler@gmial.com>
2021-05-23 03:02:19 +0300

    someFix

--- src/level2/lesson2/Main.java
+++ src/level2/lesson2/Main.java
@@ -10,7 +10,7 @@ public class Main {
         String[][] array = {
                 {"1", "2", "3", "4"},
                 {"5", "6", "7", "8"},
-                {"9", "10", "11", "1d"},
+                {"9", "10", "11", "11"},
                 {"13", "14", "15", "16"}
         };
 
@@ -29,34 +29,37 @@ public class Main {
     }
 
 
-    private static int[][] toInt(String[][] array) throws MyArraySizeException {
+    private static int[][] toInt(String[][] array) throws MyArraySizeException, MyArrayDataException {
 
-        checkValidSize(array);
+        if(!checkValidSize(array)) {
+            throw new MyArraySizeException();
+        }
 
         int[][] intArray = new int[4][4];
 
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                try {
-                    intArray[i][j] = Integer.parseInt(array[i][j]);
-                } catch (NumberFormatException ex) {
-                    throw new MyArrayDataException(++i, ++j);
-                }
+                    if (array[i][j].matches("\\d")) {
+                        intArray[i][j] = Integer.parseInt(array[i][j]);
+                    } else {
+                        throw new MyArrayDataException(++i, ++j);
+                    }
             }
         }
 
         return intArray;
     }
 
-    private static void checkValidSize(String[][] array) throws MyArraySizeException {
+    private static boolean checkValidSize(String[][] array) throws MyArraySizeException {
         if (array.length != 4) {
-            throw new MyArraySizeException();
+           return false;
         }
 
         for (String[] el : array) {
             if (el.length != 4) {
-                throw new MyArraySizeException();
+               return false;
             }
         }
+        return true;
     }
 }
